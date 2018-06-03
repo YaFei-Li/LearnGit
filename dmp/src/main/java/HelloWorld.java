3c588ff116a2b280de6535518d6aaaae4370d700
YaFei-Li <627208969@qq.com>
2018-06-03 11:15:50 +0800

    add git merge dev-lyf

--- dmp/src/main/java/HelloWorld.java
+++ dmp/src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("git checkout dev-lyf");
+        System.out.println("git merge dev-lyf");
     }
 }
