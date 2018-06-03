d5b68baccd3b5b37606f8b4cbec2ce12291b2d2f
YaFei-Li <38552689+YaFei-Li@users.noreply.github.com>
2018-06-03 11:24:25 +0800

    Delete HelloWorld.java

--- dmp/src/main/java/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("git checkout dev-lyf");
-        System.out.println("git merge dev-lyf");
-    }
-}
